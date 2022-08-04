1cb7677b7c30a9d5ef93138f17fabf02ab311568
ppotatoG <thgus2270@gmail.com>
2022-08-04 17:18:27 +0900

    add: ...

--- components/TodoListCard.tsx
+++ components/TodoListCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import styles from '../styles/Home.module.scss'
 
@@ -14,8 +14,20 @@ const TodoListCard : React.FC = () => {
     const [reviseList, setReviseList] = useState(false);
     const [todoValues, setTodoValues] = useState('');
 
-    const handleReviseList = () => {
-        setReviseList(true)
+    const handleReviseList = (type : string) : void => {
+        switch (type) {
+            case 'revise' :
+                setReviseList(true);
+                break;
+            case 'cancel' :
+                setReviseList(false);
+                break;
+            case 'submlt' :
+                //
+                break;
+            default :
+                console.log('?');
+        }
     }
 
     const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
@@ -33,17 +45,23 @@ const TodoListCard : React.FC = () => {
                 <p>{testJson.todos[0].data.title}</p>
                 {
                     reviseList
-                    && <input
+                    &&
+                    <div>
+                        <input
                             type="text"
                             placeholder="할 일을 입력하세요"
                             onChange={handleChange}
-                            value={testJson.todos[0].data.content}
+                            value={todoValues}
                         />
-                    || <p>{testJson.todos[0].data.content}</p>
+                        <a onClick={handleReviseList('submit')}>수정 후 제출</a>
+                        <a onClick={handleReviseList('cancle')}>취소</a>
+                    </div>
+                    ||
+                    <div>
+                        <p>{testJson.todos[0].data.content}</p>
+                        <a onClick={handleReviseList('revise')}>수정</a>
+                    </div>
                 }
-                <p onClick={handleReviseList}>수정</p>
-                <p>수정 후 제출</p>
-                <p>수정 후 취소</p>
             </div>
         </div>
     )
