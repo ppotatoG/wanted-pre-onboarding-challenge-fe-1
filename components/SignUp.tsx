7959ffc83cc3bfa22fb3f6569b6a6f57b08a6795
ppotatoG <thgus2270@gmail.com>
2022-08-09 16:32:20 +0900

    rfct: 피드백 반영, trim, type

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,87 +1,67 @@
-import React, {useEffect, useState, useCallback} from "react";
+import React, {useState} from "react";
 
 import styles from '../styles/Home.module.scss'
 import axios from 'axios';
 
-import {EmailPattern, PasswordPattern} from '../utils/pattern';
-import {userInfoType, FormProps} from '../types/auth'
+import {EmailPattern} from '../utils/pattern';
 
 const SignIn: React.FC = () => {
-    const [email, setEmail] = useState('');
-    const [emailError, setEmailError] = useState(false);
+    // TODO: 중복코드 common
 
-    const onChangeEmailCheck = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+    const [email, setEmail] = useState<string>('');
+    const [emailError, setEmailError] = useState<boolean>(false);
+    const [emailErrorMessage, setEmailErrorMessage] = useState<string | ''>('');
+
+    const [password, setPassword] = useState<string>('');
+    const [passwordError, setPasswordError] = useState<boolean>(false);
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState<string | ''>('');
+
+    const onChangeEmailCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
 
         setEmail(value);
-        setFormValues({...formValues, [name]: value});
+        setEmailErrorMessage(validate(name, value));
+        setEmailError(emailErrorMessage === '');
+    };
 
-        setEmailError(validate(name, value));
-    }, [email]);
-
-    const [password, setPassword] = useState('');
-
-    const [passwordError, setPasswordError] = useState(false);
-    const onChangePasswordCheck = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+    const onChangePasswordCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
 
         setPassword(value);
-        setFormValues({...formValues, [name]: value});
+        setPasswordErrorMessage(validate(name, value));
+        setPasswordError(passwordErrorMessage === '');
+    };
 
-        setPasswordError(validate(name, value));
-    }, [password]);
-
-    const [emailErrorMessage, setEmailErrorMessage] = useState('');
-    const [passwordErrorMessage, setPwErrorMessage] = useState('');
-
-    const validate = (type: string, str: string) : boolean => {
-        const errors: userInfoType = {};
-
+    const validate = (type: string, str: string): string => {
         if (type === 'email') {
             if (!str) {
-                errors.email = "이메일을 입력해주세요";
-                setEmailErrorMessage(errors.email);
-
-                return false;
+                return '이메일을 입력해주세요';
             } else if (!EmailPattern.test(str)) {
-                errors.email = "올바른 이메일을 입력해주세요.";
-                setEmailErrorMessage(errors.email);
-
-                return false;
+                return '올바른 이메일을 입력해주세요.';
             }
-
         } else if (type === 'password') {
             if (!str) {
-                errors.password = "비밀번호를 입력해주세요";
-                setPwErrorMessage(errors.password);
-
-                return false;
+                return '비밀번호를 입력해주세요';
             } else if (str.length < 8) {
-                errors.password = "올바른 비밀번호를 입력해주세요";
-                setPwErrorMessage(errors.password);
-
-                return false;
+                return '올바른 비밀번호를 입력해주세요';
             }
         }
 
-        return true;
+        return '';
     };
 
-    const [formValues, setFormValues] = useState({email:'', password:''});
-
-    const onSubmit = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
-        console.log(formValues)
-        
+
         if (emailError && passwordError) {
             axios.post('http://localhost:8080/users/create', {
-                email: formValues.email,
-                password: formValues.password
+                email: email,
+                password: password
             }).then( response => {
                 console.log(response)
                 console.log(response.data.message)
                 if (response.data.token !== '') {
-                    localStorage.setItem(`${formValues.email}`, response.data.token);
+                    localStorage.setItem(`${email}`, response.data.token);
                 }
             }).catch( error => {
                 console.log(error)
@@ -103,7 +83,8 @@ const SignIn: React.FC = () => {
                     onChange={onChangeEmailCheck}
                 />
             </div>
-            {!emailError && <p className={styles.errorMessage}>{emailErrorMessage}</p>}
+            {emailErrorMessage !== '' && <p className={styles.errorMessage}>{emailErrorMessage}</p>}
+
             <div className={styles.auth__item}>
                 <label htmlFor='password'>비밀번호</label>
                 <input
@@ -115,15 +96,15 @@ const SignIn: React.FC = () => {
                     onChange={onChangePasswordCheck}
                 />
             </div>
-            {!passwordError && <p className={styles.errorMessage}>{passwordErrorMessage}</p>}
+            {passwordErrorMessage && <p className={styles.errorMessage}>{passwordErrorMessage}</p>}
 
             <div className={styles.btn_wrap}>
-                {
-                    !emailError || !passwordError
-                        ? <button type="submit" className={styles.btn_wrap__item} style={{opacity: .5}}>회원가입</button>
-                        : <button type="submit" className={styles.btn_wrap__item}>회원가입</button>
-                }
-
+                <button
+                    disabled={!emailError || !passwordError}
+                    className={styles.btn_wrap__item}>
+                    로그인
+                </button>
+                <a className={styles.btn_wrap__item} href="../auth/SignUp">회원가입</a>
             </div>
         </form>
     )
