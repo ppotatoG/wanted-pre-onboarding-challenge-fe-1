6f1cae18f7a229f6a0804a72cba08f1f1f945295
ppotatoG <thgus2270@gmail.com>
2022-09-07 22:01:11 +0900

    feat: addCase

--- components/todo/slice.tsx
+++ components/todo/slice.tsx
@@ -1,26 +1,51 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import type { RootState } from 'components/store/store';
+import axios from 'axios';
 
 interface TodoState {
-    value: number
+    data: null | Object,
+    error: null | Object,
+    loading: boolean
 };
 
 const initialState: TodoState = {
-    value: 0
+    data: null,
+    error: null,
+    loading: false
 };
 
-export const todoSlice = createSlice({
+const getTodos = createAsyncThunk(
+    'users/fetchById',
+    // if you type your function argument here
+    async (userId: number) => {
+        const response = await axios.get(`http://localhost:8080/todos`)
+        return (await response.json()) as Returned
+    }
+)
+
+const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        increment: (state) => {
-            state.value += 1
-        },
-        decrement: (state) => {
-            state.value -= 1
-        }
+        // ...
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getTodos.pending, (state) => {
+                state.error = null,
+                state.loading = true
+            })
+            .addCase(getTodos.fulfilled, (state, { payload }) => {
+                    state.error = null,
+                    state.loading = false,
+                    state.data = payload
+            })
+            .addCase(getTodos.rejected, (state, { payload }) => {
+                    state.error = payload;
+                    state.loading = false;
+            })
     }
-})
+});
 
 export const { decrement, increment } = todoSlice.actions;
 export const selectCount = ( state: RootState ) => state.todo.value;
